4ffe647e8cb7a8c1d08d98baddf76a5a49d2bebf
Arthur Cezar <arthurcvm@hotmail.com>
2024-07-18 22:56:14 -0300

    implementação de deleção de cliente

--- src/app/clientes/clientes-lista/clientes-lista.component.ts
+++ src/app/clientes/clientes-lista/clientes-lista.component.ts
@@ -11,6 +11,9 @@ import { ClientesService } from 'src/app/clientes.service';
 export class ClientesListaComponent implements OnInit {
 
   clientes: Cliente[] = [];
+  clienteSelecionado: Cliente;
+  mensagemSucesso: String;
+  mensagemErro: String;
 
   constructor(private service: ClientesService, private router: Router) { 
 
@@ -24,6 +27,21 @@ export class ClientesListaComponent implements OnInit {
 
   novoCadastro(){
     this.router.navigate(['/clientes-form'])
+  }
+
+  preparaDelecao(cliente: Cliente){
+    this.clienteSelecionado = cliente;
+  }
+
+  deletarCliente(){
+    this.service
+    .deletar(this.clienteSelecionado)
+    .subscribe( response => {
+      this.mensagemSucesso = 'Cliente deletado com sucesso!'
+      this.ngOnInit();
+    },
+      error => this.mensagemErro = 'Ocorreu um erro ao deletar o cliente.'
+    )
   }
 
 }

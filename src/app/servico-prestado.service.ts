67a8bf0060c57c9349cabad3d338cc7a981b524e
Arthur Cezar <arthurcvm@hotmail.com>
2024-08-01 22:12:15 -0300

    implementação de listagem de serviços

--- src/app/servico-prestado.service.ts
+++ src/app/servico-prestado.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ServicoPestado } from './servico-prestado/servicoPrestado';
 import { Observable } from 'rxjs';
 import {environment} from '../environments/environment'
+import { ServicoPestadoBusca } from './servico-prestado/servico-prestado-lista/servicoPrestadoBusca';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,15 @@ export class ServicoPrestadoService {
     
   salvar(servicoPrestado: ServicoPestado) : Observable<ServicoPestado>{
     return this.http.post<ServicoPestado>(this.apiURL, servicoPrestado);
+  }
+
+  buscar(nome: string = "", mes: number) : Observable<ServicoPestadoBusca[]>{
+
+    const httpParams = new HttpParams()
+    .set("nome", nome)
+    .set("mes", mes ? mes.toString() : '');
+
+    const url = this.apiURL + "?" + httpParams.toString();
+    return this.http.get<any>(url)
   }
 }

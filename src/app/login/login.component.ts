aa77085b530877484cad2d9010226048f3a07561
Arthur Cezar <arthurcvm@hotmail.com>
2024-08-13 17:03:19 -0300

    integração de login com backend

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -23,7 +23,15 @@ export class LoginComponent {
 
 
   onSubmit(){
-    this.router.navigate(['/home'])
+    this.authService
+          .tentarLogar(this.username, this.password)
+          .subscribe(response => {
+            const access_token = JSON.stringify(response);
+            localStorage.setItem('access_token', access_token);
+            this.router.navigate(['/home'])
+          }, errorResponse => {
+            this.errors = ['Usuário e/ou senha incorreto(s).']
+          });
     
   }
 
